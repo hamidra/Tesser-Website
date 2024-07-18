abf2a32ba1c7c232656cccb57ff4b7870df667e4
hamidra <hamid.alipour@gmail.com>
2024-07-17 17:25:01 -0700

    filter unpublished

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,11 +4,13 @@ export default async function PostsList() {
   let postsMeta = await getPostsMeta().catch((err) => console.error(err))
   return (
     <div className="flex flex-row flex-wrap justify-center gap-8 p-2 md:p-16">
-      {postsMeta?.map((meta, id) => (
-        <>
-          <PostCard key={id} meta={meta} />
-        </>
-      ))}
+      {postsMeta
+        ?.filter((meta) => meta.published)
+        ?.map((meta, id) => (
+          <>
+            <PostCard key={id} meta={meta} />
+          </>
+        ))}
     </div>
   )
 }
